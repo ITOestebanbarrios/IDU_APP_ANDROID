09f13fce83d67f325a2772f1fd6e04a1a74b124f
esteban barrios <estebanbarrios@ITOestebanbarrios.local>
2018-10-29 17:33:20 -0500

    splash and login views

---
+++ collectoridu/Controllers/LoginActivity.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace collectoridu.Controllers
+{
+    /*[Activity(Label = "SplashActivity", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            this.ActionBar.Hide();
+            SetContentView(Resource.Layout.Splash);
+
+
+            // Create your application here
+        }
+    }*/
+
+    [Activity(Label = "Login Screen App", MainLauncher = true, Theme = "@style/Theme.Splash", NoHistory=true, Icon = "@mipmap/icon")]
+    public class LoginActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+			SetContentView(Resource.Layout.Login);
+            //Display Splash Screen for 4 Sec  
+            //Thread.Sleep(4000);
+            //Start Activity1 Activity  
+            //StartActivity(typeof(Activity1));
+        }
+    }
+}
---
+++ collectoridu/Controllers/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Maps;
+
+
+namespace collectoridu
+{
+    [Activity(Label = "MainActivity")]
+    public class MainActivity : Activity, IOnMapReadyCallback
+    {
+        //int count = 1;
+
+        private GoogleMap GMap;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            /*Button button = FindViewById<Button>(Resource.Id.myButton);
+            button.Click += delegate { 
+                button.Text = $"{count++} clicks!"; 
+            };*/
+            SetUpMap();
+        }
+
+        private void SetUpMap()
+        {
+            if (GMap == null)
+            {
+                FragmentManager.FindFragmentById<MapFragment>(Resource.Id.googlemap).GetMapAsync(this);
+            }
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            this.GMap = googleMap;
+        }
+    }
+}
+
