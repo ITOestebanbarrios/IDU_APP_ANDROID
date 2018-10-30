9a4c8b789ba1c0288f27b1a2edadd1abf7c79b2a
esteban barrios <estebanbarrios@ITOestebanbarrios.local>
2018-10-30 11:16:00 -0500

    ok

--- collectoridu/Controllers/LoginActivity.cs
+++ collectoridu/Controllers/LoginActivity.cs
@@ -10,24 +10,11 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Util;
+//using System.Diagnostics;
 
 namespace collectoridu.Controllers
 {
-    /*[Activity(Label = "SplashActivity", MainLauncher = true, Icon = "@mipmap/icon")]
-    public class SplashActivity : Activity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-
-            // Set our view from the "main" layout resource
-            this.ActionBar.Hide();
-            SetContentView(Resource.Layout.Splash);
-
-
-            // Create your application here
-        }
-    }*/
 
     [Activity(Label = "Login Screen App", MainLauncher = true, Theme = "@style/Theme.Splash", NoHistory=true, Icon = "@mipmap/icon")]
     public class LoginActivity : Activity
@@ -35,8 +22,27 @@ namespace collectoridu.Controllers
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
 			SetContentView(Resource.Layout.Login);
+
+
+            Button button = FindViewById<Button>(Resource.Id.login_iniciar);
+            TextView olvidasteUsuario = FindViewById<TextView>(Resource.Id.olvidaste_usuario);
+            TextView olvidasteContrasena = FindViewById<TextView>(Resource.Id.olvidaste_contrasena);
+
+            button.Click += delegate {
+                Log.Warn("YES", "ok_ok_ok_ok_ok_ok_ok_ok");
+            };
+
+            olvidasteUsuario.Click += delegate {
+                StartActivity(typeof(OlvidasteActivity));
+            };
+
+            olvidasteContrasena.Click += delegate {
+                StartActivity(typeof(OlvidasteActivity));
+            };
+
+
+
             //Display Splash Screen for 4 Sec  
             //Thread.Sleep(4000);
             //Start Activity1 Activity  
